cb83535e620273c66607b15879681b290fe6ddad
LollaManolla <lollamanolla@gmail.com>
2015-03-15 15:19:28 +0100

    index

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+//scroll to top
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
